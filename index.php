48016d71d149f4e26259ac437f7fb2a02e1bcb7b
Delermando <delsantos@hotmail.com.br>
2014-12-17 20:14:05 -0200

    arquivos netbeans

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 
-    require_once('vendor/autoload.php');
-    require_once('controllers/control.php');
-    $CardController->render();
+//    require_once('vendor/autoload.php');
+//    require_once('controllers/control.php');
+//    $CardController->render();
     
+    echo 'pasta system agenda v3';
     
