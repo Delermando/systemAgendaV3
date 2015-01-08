144410b84ce95532585e87bc0559299079bb0645
Delermando <delsantos@hotmail.com.br>
2015-01-08 15:58:26 -0200

    refactor, criado dominios, para o angulor e o views, instalado o node, e o bower, e outras dependencias, restruturadas as pastas

---
+++ apiAgenda/index.php
@@ -0,0 +1,18 @@
+<?php
+
+ob_start();
+session_start();
+header('Content-Type: text/html; charset=utf-8');
+ini_set('display_errors', 'on');
+error_reporting(E_ALL);
+ini_set("log_errors", 1);
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$app = new Silex\Application();
+$app['debug'] = true;
+$app->register(new Silex\Provider\ServiceControllerServiceProvider());
+
+require_once ('services/routes/cardRoutes.php');
+
+$app->run();
---
+++ apiAgenda/services/controllers/CardController.php
@@ -0,0 +1,49 @@
+<?php namespace Cartao\services\controllers;
+    
+class CardController {
+    private $card;
+    private $validate;
+    private $response;
+
+    public function __construct() {
+        $this->card = new \Cartao\services\model\card\CardModel();
+        $this->validate = new \Cartao\services\validate\Validate();
+        $this->response = new \Cartao\services\response\Response();
+    }
+    
+    public function save($toSave){
+          $this->validate->isEmail($toSave['fromEmail']);
+          $this->validate->isEmail($toSave['toEmail']);
+          $this->response->message = $this->validate->getError();
+          $this->response->status = $this->validate->getStatusValitadion();
+          $this->response->data = $this->saveIfValidationIsTrue($toSave);
+          return $this->response->json();  
+    }
+    
+    public function select(){
+        $this->response->data = $this->card->select();
+        return $this->response->json();
+    }
+    
+    public function delete($idToDelete){
+        $this->response->data = $this->deleteIfIdIsInt($idToDelete);
+        return $this->response->json();
+    }
+    
+    public function update($arrayToUpdate){
+        $this->response->data = $this->card->update($arrayToUpdate);
+        return $this->response->json();
+    }
+
+    private function saveIfValidationIsTrue($arrayToSave){
+        if($this->validate->getStatusValitadion()){
+            return $this->response->data = $this->card->save($arrayToSave);
+        }
+    }
+    private function deleteIfIdIsInt($idToDelete){
+        if($this->validate->getStatusValitadion()){
+            return $this->response->data = $this->card->delete($idToDelete);
+        }
+    }
+
+}
