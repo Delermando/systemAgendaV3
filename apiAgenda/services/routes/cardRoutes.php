3f162106dcf56f3510cca45405db44703a95c0aa
Delermando <delsantos@hotmail.com.br>
2015-01-16 20:11:12 -0200

    terminada documentacao

--- apiAgenda/services/routes/cardRoutes.php
+++ apiAgenda/services/routes/cardRoutes.php
@@ -11,12 +11,11 @@ $app['card.route'] = $app->share(function() use ($app) {
  *  @SWG\Operations(
  *      @SWG\Operation(
  *          method="GET", 
- *          nickname="home"
+ *          nickname="home",
  *      )
  *  )
  *)
  */
-
 $app->get('/', 'card.route:home');
 
 /**
@@ -26,11 +25,12 @@ $app->get('/', 'card.route:home');
  *  @SWG\Operations(
  *      @SWG\Operation(
  *          method="GET", 
- *          nickname="listAllCards"
+ *          nickname="listAllCards",
  *      )
  *  )
  *)
  */
+
 $app->get('/v1/cards/list', 'card.route:cardList');
 /**
  * @SWG\Api(
@@ -41,14 +41,14 @@ $app->get('/v1/cards/list', 'card.route:cardList');
  *          nickname="searchUnicCard",
  *          @SWG\Parameter(
  *              name="idCard",
- *              description="ID do card",
+ *              description="Object com o ID do card a ser pesquisado",
  *              paramType="path",
  *              required=true,
  *              allowMultiple=false,
  *              type="integer",
- *              defaultValue=308
+ *              defaultValue=300
  *          ),
- *          @SWG\ResponseMessage(code=404, message="null"),
+ *          @SWG\ResponseMessage(code=01, message="noRegisters"),
  *      )
  *  )
  *)
@@ -63,23 +63,93 @@ $app->get('/v1/cards/list/{idCard}', 'card.route:getUnicCard');
  *          method="POST", 
  *          nickname="deleteCard",
  *          @SWG\Parameter(
- *              name="idCard",
- *              description="ID do card",
+ *              name="jsonDelete",
+ *              description="Object com o ID do card a ser deletado",
  *              paramType="body",
  *              required=true,
  *              allowMultiple=true,
- *              type="idCard"
+ *              type="objectToDelete"
  *          ),
- *          @SWG\ResponseMessage(code=404, message="null"),
+ *          @SWG\ResponseMessage(code=00, message="no error"),
+ *          @SWG\ResponseMessage(code=01, message="noRegisters"),
  *      ),
- *  
  *  ),
  *)
- */
-/**
- * @SWG\Model(id="idCard",required="true")
+ * @SWG\Model(id="objectToDelete",required="true")
  * @SWG\Property(name="idCard",type="array",description="Array de ids para deletar", @SWG\Items("integer"))
  */
 $app->post('/v1/cards/delete', 'card.route:cardDelete');
+
+/**
+ * @SWG\Api(
+ *  path="/v1/cards/save", 
+ *  @SWG\Operations(
+ *      @SWG\Operation(
+ *          method="POST", 
+ *          nickname="saveCard",
+ *          @SWG\Parameter(
+ *              name="jsonSave",
+ *              description="Objeto/Array com os IDs a serem deletados",
+ *              paramType="body",
+ *              required=true,
+ *              allowMultiple=true,
+ *              type="objectToSave"
+ *          ),
+ *          @SWG\ResponseMessage(code=00, message="no error"),
+ *          @SWG\ResponseMessage(code=01, message="wrongParamName"),
+ *          @SWG\ResponseMessage(code=02, message="wrongNumbOfParams"),
+ *          @SWG\ResponseMessage(code=03, message="wrongNumbOfParams"),
+ *          @SWG\ResponseMessage(code=04, message="paramNotSet"),
+ *          @SWG\ResponseMessage(code=05, message="paramNotSet"),
+ *          @SWG\ResponseMessage(code=06, message="wrongEmailFormat"),
+ *          @SWG\ResponseMessage(code=07, message="wrongDateFormat"),
+ *      ),
+ *  ),
+ *)
+ * @SWG\Model(id="objectToSave",required="true")
+ * @SWG\Property(name="fromEmail",type="string",description="E-mail do remetente"))
+ * @SWG\Property(name="toEmail",type="string",description="E-mail do destinatario")
+ * @SWG\Property(name="fromName",type="string",description="Nome do remetente")
+ * @SWG\Property(name="toName",type="string",description="Nome do destinatario")
+ * @SWG\Property(name="message",type="string",description="Mensagem para o destinatario")
+ * @SWG\Property(name="date",type="string",description="Data para o envio do cartao")
+ */
 $app->post('/v1/cards/save', 'card.route:cardSave');
+
+/**
+ * @SWG\Api(
+ *  path="/v1/cards/update", 
+ *  @SWG\Operations(
+ *      @SWG\Operation(
+ *          method="POST", 
+ *          nickname="updateCard",
+ *          @SWG\Parameter(
+ *              name="jsonUpdate",
+ *              description="Object/List com os dados as serem atualizados",
+ *              paramType="body",
+ *              required=true,
+ *              allowMultiple=true,
+ *              type="objectToUpdate"
+ *          ),
+ *          @SWG\ResponseMessage(code=00, message="no error"),
+ *          @SWG\ResponseMessage(code=01, message="wrongParamName"),
+ *          @SWG\ResponseMessage(code=02, message="wrongNumbOfParams"),
+ *          @SWG\ResponseMessage(code=03, message="wrongNumbOfParams"),
+ *          @SWG\ResponseMessage(code=04, message="paramNotSet"),
+ *          @SWG\ResponseMessage(code=05, message="paramNotSet"),
+ *          @SWG\ResponseMessage(code=06, message="wrongEmailFormat"),
+ *          @SWG\ResponseMessage(code=07, message="wrongDateFormat"),
+ *          @SWG\ResponseMessage(code=07, message="noRegisters"),
+ *      ),
+ *  ),
+ *)
+ * @SWG\Model(id="objectToUpdate",required="true")
+ * @SWG\Property(name="idCard",type="integer",description="ID do card a ser atualizado"))
+ * @SWG\Property(name="fromEmail",type="string",description="E-mail do remetente"))
+ * @SWG\Property(name="toEmail",type="string",description="E-mail do destinatario")
+ * @SWG\Property(name="fromName",type="string",description="Nome do remetente")
+ * @SWG\Property(name="toName",type="string",description="Nome do destinatario")
+ * @SWG\Property(name="message",type="string",description="Mensagem para o destinatario")
+ * @SWG\Property(name="date",type="string",description="Data para o envio do cartao")
+ */
 $app->post('/v1/cards/update', 'card.route:cardUpdate');
